a97350aad4eb7f57a9025a5cd07e45e877cf4658
Harshit Vasu <harshitvasu@gmail.com>
2020-10-29 13:59:47 +0530

    minor

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ let capabilities = {
 	"browserstack.user" : username,
 	"browserstack.key" : accessKey
 };
-
+console.log(process.env);
 let driver = new webdriver.Builder().
   usingServer("https://hub-cloud.browserstack.com/wd/hub").
   withCapabilities(capabilities).
