d1282238289e05d2c7f877ad3a70e9b1e876a39f
harshitvasu <60222330+harshitvasu@users.noreply.github.com>
2021-02-17 19:32:27 +0530

    Update test.js
    Additional changesr

--- test.js
+++ test.js
@@ -5,36 +5,29 @@ let buildName = process.env.BROWSERSTACK_BUILD_NAME;
 let browserstackLocal = process.env.BROWSERSTACK_LOCAL;
 let browserstackLocalIdentifier = process.env.BROWSERSTACK_LOCAL_IDENTIFIER;
 
-let capabilities = {
-	"os" : "Windows",
-	"os_version" : "10",
-	"browserName" : "chrome",
-	"browser_version" : "latest",
-	"name": "BStack -[Jenkins] Sample Test", // test name
-	"build" : buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env letiable
-	"browserstack.local" : browserstackLocal,
-	"browserstack.localIdentifier" : browserstackLocalIdentifier,
-	"browserstack.user" : username,
-	"browserstack.key" : accessKey
-};
-console.log(process.env);
-let driver = new webdriver.Builder().
-  usingServer("https://hub-cloud.browserstack.com/wd/hub").
-  withCapabilities(capabilities).
-  build();
 
-driver.get('https://www.google.com').then(function(){
-  driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack').then(function(){
-    driver.getTitle().then(function(title) {
-      console.log(title);
-    });
-  });
-}); 
-if(browserstackLocal){
-	driver.get('http://localhost:8080').then(function(){
- 	driver.getTitle().then(function(title) {
-	      console.log(title);
-	      driver.quit();
-	    });
-	}); 
+const runTest = async ()=>{
+  let capabilities = {
+    "os" : "Windows",
+    "os_version" : "10",
+    "browserName" : "chrome",
+    "browser_version" : "latest",
+    "name": "BStack -[Jenkins] Sample Test", // test name
+    "build" : buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env letiable
+    "browserstack.local" : browserstackLocal,
+    "browserstack.localIdentifier" : browserstackLocalIdentifier,
+    "browserstack.user" : username,
+    "browserstack.key" : accessKey
+  };
+  console.log(process.env);
+  let driver = new webdriver.Builder().
+    usingServer("https://hub-cloud.browserstack.com/wd/hub").
+    withCapabilities(capabilities).
+    build();
+  await driver.get('https://www.google.com');
+  console.log(await driver.getTitle());
+  await driver.get('https://localhost:4445');
+  console.log(await driver.getTitle());
+  await driver.quit();
 }
+runTest();
